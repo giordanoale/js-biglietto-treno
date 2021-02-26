317ccbd5063b8dc8bacb03f7f7f23ecb57869772
giordanoale <a.giordano89@gmail.com>
2021-02-26 17:25:17 +0100

    Aggiunto controllo per l'inserimento dei dati

--- js/script.js
+++ js/script.js
@@ -5,20 +5,33 @@ alert ("Benvenuto su Raileean, per calcolare il prezzo del biglietto del treno a
 
 var chilometri = prompt("Inserisci il numero di chilometri che vuoi percorrere");
 
+chilometri = parseInt(chilometri);
+
+console.log(isNaN(chilometri));
+
+if (isNaN(chilometri)) {
+  alert ("Inserisci il numero dei chilometri, non scriverlo a lettere! Refresha la pagina e riprova!");
+}
+
 // 2. Calcolo del prezzo per chilometro
 var prezzo = chilometri * 0.21;
-console.log(prezzo);
+console.log("Prezzo del biglietto al netto degli sconti: " + prezzo + " €");
 
 // 3. Inserimento età utente
 var eta = prompt("Inserisci la tua età");
 
+console.log(isNaN(eta));
+
+if (isNaN(eta)) {
+  alert ("Inserisci il numero dei chilometri, non scriverlo a lettere! Refresha la pagina e riprova!");
+}
+
 // 4. Prezzo totale viaggio
 if ( eta < 18 ) {
-  var scontoMin = prezzo - ((prezzo * 20) / 100);
-  document.getElementById("prezzofinale").innerHTML = scontoMin + "€";
-} else if ( eta >= 65 ) {
-  var scontoOld = prezzo - ((prezzo * 40) / 100);
-  document.getElementById("prezzofinale").innerHTML = scontoOld + "€";
-} else {
-  document.getElementById("prezzofinale").innerHTML = prezzo + "€";
+  prezzo = prezzo - ((prezzo * 20) / 100);
 }
+if ( eta >= 65 ) {
+  prezzo = prezzo - ((prezzo * 40) / 100);
+}
+
+document.getElementById("prezzofinale").innerHTML = prezzo + " €";
