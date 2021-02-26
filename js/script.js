c3de04111643834f15fde9cc2c5500c3fa43e8c4
giordanoale <a.giordano89@gmail.com>
2021-02-26 16:30:29 +0100

    Le condizioni mostrano il risultato sulla pagina web

--- js/script.js
+++ js/script.js
@@ -1,21 +1,24 @@
 
 // 1. Inserimento Chilometri viaggio
+
+alert ("Benvenuto su Raileean, per calcolare il prezzo del biglietto del treno abbiamo bisogno di un paio di informazioni. Sei pronto?")
+
 var chilometri = prompt("Inserisci il numero di chilometri che vuoi percorrere");
 
+// 2. Calcolo del prezzo per chilometro
 var prezzo = chilometri * 0.21;
 console.log(prezzo);
 
-// 2. Inserimento età utente
+// 3. Inserimento età utente
 var eta = prompt("Inserisci la tua età");
 
-// 3. Prezzo totale viaggio
-
+// 4. Prezzo totale viaggio
 if ( eta < 18 ) {
   var scontoMin = prezzo - ((prezzo * 20) / 100);
-  console.log(scontoMin);
+  document.getElementById("prezzofinale").innerHTML = scontoMin + "€";
 } else if ( eta >= 65 ) {
   var scontoOld = prezzo - ((prezzo * 40) / 100);
-  console.log(scontoOld);
+  document.getElementById("prezzofinale").innerHTML = scontoOld + "€";
 } else {
-  console.log(prezzo);
+  document.getElementById("prezzofinale").innerHTML = prezzo + "€";
 }
